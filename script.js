3e693a3fec52e5ac15c3424a367c39d0dcf3ef55
danielpradolima-debug <daniel.prado.lima@escola.pr.gov.br>
2026-07-02 19:49:36 -0300

    segunda versão

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("Projeto carregado!");
+// Tamanho inicial da fonte
+let tamanhoFonte = 18;
+
+// Alto contraste
+document.getElementById("contraste").addEventListener("click", () => {
+    document.body.classList.toggle("alto-contraste");
+});
+
+// Aumentar fonte
+document.getElementById("aumentar").addEventListener("click", () => {
+    tamanhoFonte += 2;
+    document.body.style.fontSize = tamanhoFonte + "px";
+});
+
+// Diminuir fonte
+document.getElementById("diminuir").addEventListener("click", () => {
+    if (tamanhoFonte > 12) {
+        tamanhoFonte -= 2;
+        document.body.style.fontSize = tamanhoFonte + "px";
+    }
+});
+
+// Leitura da página
+document.getElementById("ler").addEventListener("click", () => {
+    speechSynthesis.cancel();
+
+    const texto = document.querySelector("main").innerText;
+
+    const fala = new SpeechSynthesisUtterance(texto);
+    fala.lang = "pt-BR";
+    fala.rate = 1;
+
+    speechSynthesis.speak(fala);
+});
